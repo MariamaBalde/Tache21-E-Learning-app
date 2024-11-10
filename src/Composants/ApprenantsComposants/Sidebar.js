cfe683e38adb4d2cb96ee4fdd4584f86a221f143
Mariama Balde <foulah18@gmail.com>
2024-11-10 18:36:27 +0000

    sidebar modif

--- src/Composants/ApprenantsComposants/Sidebar.js
+++ src/Composants/ApprenantsComposants/Sidebar.js
@@ -6,9 +6,9 @@ function Sidebar() {
       <aside className="sidebar">
           <ul>
               <li><Link to="/dashboard">Dashboard</Link></li>
+              <li><Link to="/cours">Cours</Link></li>
               <li><Link to="/livraisons">Livraisons</Link></li>
-              <li><Link to="/certificats">Certificats</Link></li>
-              <li><Link to="/messagerie">Messagerie</Link></li>
+              <li><Link to="/tache">Taches</Link></li>
           </ul>
       </aside>
   )
