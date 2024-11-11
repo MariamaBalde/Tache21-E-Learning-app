c1cd072c7a88df1b0f2d891f05330ab9d4ca05f9
Mariama Balde <foulah18@gmail.com>
2024-11-11 16:02:23 +0000

    all change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from "./Composants/ApprenantsComposants/Navbar";
 import Sidebar from "./Composants/ApprenantsComposants/Sidebar";
 import Dashboard from './Composants/ApprenantsComposants/Dashboard';
-import Cours from './Composants/ApprenantsComposants/Cours';
+import CoursApp from './Composants/ApprenantsComposants/CoursApp';
 import Livraisons from './Composants/ApprenantsComposants/Livraisons';
 import Taches from './Composants/ApprenantsComposants/Taches';
 
@@ -11,14 +11,14 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar/>
+        {/* <Navbar/> */}
         <div className="main-content">
-          <Sidebar/>
+          {/* <Sidebar/> */}
           <div className="page-content">
             <Routes>
               <Route path='/' element={<Dashboard />} />
               <Route path='/dashboard' element={<Dashboard />} />
-              <Route path='/cours' element={<Cours />} />
+              <Route path='/coursapp' element={<CoursApp />} />
               <Route path='/livraisons' element={<Livraisons />} />
               <Route path='/taches' element={<Taches />} />
             </Routes>
--- src/Composants/ApprenantsComposants/Taches.js
+++ src/Composants/ApprenantsComposants/Taches.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export const Taches = () => {
+ const Taches = () => {
   const task = {
     number: "1",
     duration: "168h",
@@ -130,3 +130,5 @@ export const Taches = () => {
     </div>
   );
 };
+
+export default Taches;
