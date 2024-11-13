304cfa84e80b973146b0a8a5a5d6125c80bcebad
Moustaphakonté <kontemoustapha1996@gmail.com>
2024-11-13 14:09:41 +0100

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-import './App.css';
-import { Taches } from './Composants/ApprenantsComposants/Taches';
-
-function App() {
-  return (
-    <div className="App">
-      <Taches/>
-    </div>
-=======
-import React from 'react';
-=======
->>>>>>> e58804233b289104af5a665e1e8287a14ec6cb0b
-=======
 
 import React, { useState } from 'react';
->>>>>>> fffe4716c23b9620c7fbf797a8b7edec2503af9e
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from "./Composants/ApprenantsComposants/Navbar";
@@ -50,15 +32,7 @@ function App() {
         </div>
       </div>
     </Router>
-<<<<<<< HEAD
->>>>>>> d280d2b8a6f1da271a969441b9fa74b824610e6e
-=======
->>>>>>> e58804233b289104af5a665e1e8287a14ec6cb0b
   );
 }
 
 export default App;
-
-
-
-
