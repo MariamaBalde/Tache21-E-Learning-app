5d01b9ee8a41dae8c109d4a1503765829e250eb8
Ousmane DIATTA <ousmanediatta273@gmail.com>
2024-11-21 10:25:38 +0000

    suprimer

--- src/Composants/ApprenantsComposants/CoursApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CoursApp = () => {
-  return (
-    <div>CoursApp</div>
-  )
-}
-
-export default CoursApp
--- src/Composants/CoachComposants/Domaines.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Domaines = () => {
-  return (
-    <div>Domaines</div>
-  )
-}
-
-export default Domaines
