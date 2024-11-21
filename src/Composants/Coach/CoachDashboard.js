ae7c7793c96d5f2ecae44fcfb4d715e71581ea64
mamadou doucoure <doucouresoma10@gmail.com>
2024-11-21 13:52:01 +0000

    modif

--- src/Composants/Coach/CoachDashboard.js
+++ src/Composants/Coach/CoachDashboard.js
@@ -6,7 +6,9 @@ const CoachDashboard = () => {
     <div className="p-4">
       <h1 className="text-3xl font-bold">Coach Dashboard</h1>
       <nav className="my-4 space-x-4">
-        <Link to="/coach/domains" className="text-blue-500">Domains</Link>
+        <Link to="/coach/domains" className="text-blue-500">
+        
+        </Link>
         <Link to="/coach/subdomains" className="text-blue-500">Subdomains</Link>
         <Link to="/coach/courses" className="text-blue-500">Courses</Link>
         <Link to="/coach/quizzes" className="text-blue-500">Quizzes</Link>
