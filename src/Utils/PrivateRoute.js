21f1680f336f5dbd160b389f604e984be4adf537
Fatoumata binta Diallo <bined0599@gmail.com>
2024-11-25 16:50:13 +0000

    pullage de l'element1

--- src/Utils/PrivateRoute.js
+++ src/Utils/PrivateRoute.js
@@ -62,7 +62,7 @@
 
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAuth } from '../Hooks/useAuth';
+import { useAuth } from '../Hooks/UseAuth';
 
 const PrivateRoute = ({ children, roleRequired }) => {
   const { user, loading } = useAuth();
