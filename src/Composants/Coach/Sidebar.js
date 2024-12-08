d9c8ce54c006935dec59c381f53ed546db80f366
Mariama Balde <foulah18@gmail.com>
2024-12-08 09:55:28 +0000

    ajout du pull de elements3

--- src/Composants/Coach/Sidebar.js
+++ src/Composants/Coach/Sidebar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { FiMenu, FiX, FiHome, FiBookOpen, FiLayers, FiClipboard, FiHelpCircle } from 'react-icons/fi';
 import { FaPaperPlane, FaProjectDiagram, FaEnvelope, } from 'react-icons/fa';
 
-import logo from '../../Images/ImagesAdmin/1.png';
+import logo from '../../Images/ImagesAdmin/logo2.png';
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -34,10 +34,10 @@ const Sidebar = () => {
             )}
 
             <div
-                className={`fixed top-0 left-0 h-full w-42 p-4 text-white text-lg font-bold bg-[#191970] transform transition-transform duration-300 ease-in-out z-20 ${isOpen ? 'translate-x-0' : '-translate-x-full'
+                className={`fixed top-0 left-0 h-full w-42 p-4 text-white  font-bold bg-[#191970] transform transition-transform duration-300 ease-in-out z-20 ${isOpen ? 'translate-x-0' : '-translate-x-full'
                     } lg:translate-x-0 lg:static lg:h-screen lg:w-56`}
             >
-                <img src={logo} alt="Logo" className="h-12 w-12 mb-6 mx-auto" />
+                <img src={logo} alt="Logo" className="h-16 w-16 mb-6  mr-2" />
                 <nav>
                     <ul>
                         <li>
