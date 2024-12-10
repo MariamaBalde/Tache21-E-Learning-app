1d93aca85e87c2feda30433c9eaec1536232592c
unknown <you@example.com>
2024-12-10 10:40:05 +0100

    je pull

---
+++ src/Composants/Coach/Livraisons.js
@@ -0,0 +1,8 @@
+const Livraisons = () => {
+    return   <div className="p-6">
+    <h2 className="text-2xl font-bold text-gray-800 mb-4">Messagerie</h2>
+    <p className="text-gray-600">Liste des conversations avec les étudiants...</p>
+</div>;
+}
+ 
+export default Livraisons;
